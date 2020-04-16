b463be72310cc461d33986ac6d0b943925cc0919
Stiven Trujillo <strujillo1293@gmail.com>
2020-04-16 13:30:28 -0500

    implement bootstrap

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+
   </React.StrictMode>,
   document.getElementById('root')
 );
